2f36723f02d92e175b4d5c8b95f63c89dae8de85
Félix Saparelli <me@passcod.name>
2016-08-03 15:22:17 +1200

    Add message if empty

--- src/js/components/today.js
+++ src/js/components/today.js
@@ -4,7 +4,9 @@ import React from 'react'
 import TimeRow from './time-row'
 
 function Today ({ on, today }) {
-  let i = 0 // A bit obscure, but i++ === 0 only once, i.e. 'first row'
+  let i = 0
+  // A bit obscure: i++ === 0 only once, i.e. on the first iteration,
+  // and if i === 0 *after* the loop, then it hasn't run (empty array).
 
   return <section className='today'>
     {today.toSeq().reverse().map((entry, ts) =>
@@ -14,6 +16,7 @@ function Today ({ on, today }) {
         live={on && i++ === 0}
         ts={ts} />
     ).toArray()}
+    {i === 0 ? <p className='empty'>Nothing there!</p> : null}
   </section>
 }
 
