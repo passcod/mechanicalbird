83c32ee77fa6a8d9c6269d1aebf3107386f8eb1a
Félix Saparelli <me@passcod.name>
2016-08-06 16:36:24 +1200

    Use Text{De,En}coder polyfill when needed

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,7 @@
 import 'domready'
 import 'immutable'
 import 'moment'
+
+import { TextDecoder, TextEncoder } from 'text-encoding'
+if (!('TextDecoder' in window)) { Object.assign(window, { TextDecoder }) }
+if (!('TextEncoder' in window)) { Object.assign(window, { TextEncoder }) }
